3af90a1c5fd0b4b731556d2473df63e9900c671c
segaderiveraadrian <segaderiveraadrian@gmail.com>
2022-11-14 18:30:21 +0100

    actualizacion general

--- dolor.js
+++ dolor.js
@@ -1,17 +1,13 @@
 "use strict";
 
-console.log("Hola!");
+const botonesBateria = document.querySelectorAll(".bateria");
 
-const audio = new Audio("./sonido/crash.wav");
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    audio.play();
-  });
-});
-
-function play() {
-  var audio = document.getElementById("audio");
-  audio.play();
-}
+const crash = new Audio("./sonido/crash.wav");
+const hatClose = new Audio("./sonido/hithat-close.wav");
+const hatOpen = new Audio("./sonido/hithat-open.wav");
+const kick = new Audio("./sonido/kick.wav");
+const ride = new Audio("./sonido/ride.wav");
+const snare = new Audio("./sonido/snare.wav");
+const tomLow = new Audio("./sonido/tom-low.wav");
+const tomMid = new Audio("./sonido/tom-mid.wav");
+const tomHigh = new Audio("./sonido/tom-high.wav");
