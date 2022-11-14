fbad99ddb33a411f953092fb1de8b0dd916031a5
Loromor <loiromor2108@gmail.com>
2022-11-14 19:38:07 +0100

    añadido el event para todas las teclas

--- dolor.js
+++ dolor.js
@@ -2,10 +2,58 @@
 
 let numberOfDrumButtons = document.querySelectorAll(".bateria").length;
 const crashC = document.querySelector(".a");
+const hitClose = document.querySelector(".s");
+const hitOpen = document.querySelector(".d");
+const kickS = document.querySelector(".f");
+const rideS = document.querySelector(".g");
+const snareS = document.querySelector(".h");
+const tomHigh = document.querySelector(".j");
+const tomLow = document.querySelector(".k");
+const tomMid = document.querySelector(".l");
 
 crashC.addEventListener("click", (e) => {
   const crashC2 = new Audio("sonido/crash.wav");
   crashC2.play();
+});
+
+hitClose.addEventListener("click", (e) => {
+  const hitClose = new Audio("sonido/hihat-close.wav");
+  hitClose.play();
+});
+
+hitOpen.addEventListener("click", (e) => {
+  const hitOpen = new Audio("sonido/hihat-open.wav");
+  hitOpen.play();
+});
+
+kickS.addEventListener("click", (e) => {
+  const kickS = new Audio("sonido/kick.wav");
+  kickS.play();
+});
+
+rideS.addEventListener("click", (e) => {
+  const rideS = new Audio("sonido/ride.wav");
+  rideS.play();
+});
+
+snareS.addEventListener("click", (e) => {
+  const snareS = new Audio("sonido/snare.wav");
+  snareS.play();
+});
+
+tomHigh.addEventListener("click", (e) => {
+  const tomHigh = new Audio("sonido/tom-high.wav");
+  tomHigh.play();
+});
+
+tomLow.addEventListener("click", (e) => {
+  const tomLow = new Audio("sonido/tom-low.wav");
+  tomLow.play();
+});
+
+tomMid.addEventListener("click", (e) => {
+  const tomMid = new Audio("sonido/tom-mid.wav");
+  tomMid.play();
 });
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
@@ -33,7 +81,7 @@ function makeSound(key) {
       kick.play();
       break;
     case "g":
-      const ride = new Audio("sonido/ride,wav");
+      const ride = new Audio("sonido/ride.wav");
       ride.play();
       break;
     case "h":
@@ -49,7 +97,7 @@ function makeSound(key) {
       tomMid.play();
       break;
     case "l":
-      const tomHigh = new Audio("sonido/tom.high.wav");
+      const tomHigh = new Audio("sonido/tom-high.wav");
       tomHigh.play();
       break;
     default:
