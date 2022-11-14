b389587443ce4721d95ef212ce484dc950c0d54c
segaderiveraadrian <segaderiveraadrian@gmail.com>
2022-11-14 17:14:04 +0100

    dolor

--- dolor.js
+++ dolor.js
@@ -1,3 +1,12 @@
 "use strict";
 
 console.log("Hola!");
+
+const audio = new Audio(".Bateria-Hackaboss/sonido/crash.wav");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    Audio.play;
+  });
+});
