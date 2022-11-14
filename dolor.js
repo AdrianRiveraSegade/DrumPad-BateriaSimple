5ae31a3ec9ac4a771b1eea5b756ac56c339307c4
segaderiveraadrian <segaderiveraadrian@gmail.com>
2022-11-14 18:46:40 +0100

    mequieromorir

--- dolor.js
+++ dolor.js
@@ -11,3 +11,27 @@ const snare = new Audio("./sonido/snare.wav");
 const tomLow = new Audio("./sonido/tom-low.wav");
 const tomMid = new Audio("./sonido/tom-mid.wav");
 const tomHigh = new Audio("./sonido/tom-high.wav");
+
+for (let i = 0; i < botonesBateria; i++) {
+  document
+    .querySelectorAll(".bateria")
+    [i].addEventListener("click", function () {
+      const buttonInnerHTML = this.innerHTML;
+      makeSound(buttonInnerHTML);
+    });
+
+  document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+  });
+}
+
+function makeSound(key) {
+  switch (key) {
+    case "a":
+      crash.play();
+      break;
+    case "s":
+      hatClose.play();
+      break;
+  }
+}
