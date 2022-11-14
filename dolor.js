98cb05b17a56c0b6245752bbe46a87557441f67c
Loromor <loiromor2108@gmail.com>
2022-11-14 17:51:44 +0100

    DIOS SANTO SUENA

--- dolor.js
+++ dolor.js
@@ -2,11 +2,16 @@
 
 console.log("Hola!");
 
-const audio = new Audio(".Bateria-Hackaboss/sonido/crash.wav");
+const audio = new Audio("./sonido/crash.wav");
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    Audio.play;
+    audio.play();
   });
 });
+
+function play() {
+  var audio = document.getElementById("audio");
+  audio.play();
+}
